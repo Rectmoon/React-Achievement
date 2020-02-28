4bf13e676633330e1261a62b74d1c4b7e76da65a
Rectmoon <15692087320@163.com>
2020-02-29 00:08:55 +0800

    ~

--- src/react/index.js
+++ src/react/index.js
@@ -8,6 +8,34 @@ function createElement(type, props, ...children) {
   }
 }
 
+!(function() {
+  // hooks logic
+
+  let memoizedState = [] // hooks 存放在这个数组
+  let cursor = 0 // 当前 memoizedState 下标
+
+  function useState(initialValue) {
+    memoizedState[cursor] = memoizedState[cursor] || initialValue
+    const currentCursor = cursor
+    function setState(newState) {
+      memoizedState[currentCursor] = newState
+      render()
+    }
+    return [memoizedState[cursor++], setState] // 返回当前 state，并把 cursor 加 1
+  }
+
+  function useEffect(callback, depArray) {
+    const hasNoDeps = !depArray
+    const deps = memoizedState[cursor]
+    const hasChangedDeps = deps ? !depArray.every((el, i) => el === deps[i]) : true
+    if (hasNoDeps || hasChangedDeps) {
+      callback()
+      memoizedState[cursor] = depArray
+    }
+    cursor++
+  }
+})()
+
 export class Component {
   constructor(props = {}) {
     this.state = {}
@@ -20,5 +48,7 @@ export class Component {
 }
 
 export default {
-  createElement
+  createElement,
+  useState,
+  useEffect
 }
